0d41ebf716b3f7fbe368bad979e47263e5fcf3a3
lms8262 <zergling8262@gmail.com>
2023-08-01 15:04:33 +0900

    up

---
+++ src/main/java/com/example/final_team_project/FinalTeamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.final_team_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalTeamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalTeamProjectApplication.class, args);
+	}
+
+}
